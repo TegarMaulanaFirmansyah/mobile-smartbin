f583a394a39fdd87bc85c1ebe51e5a042b45dea0
fuadadhim24 <fuadadhim24@gmail.com>
2024-11-19 20:04:50 +0700

    feat: add sign in page and theme

--- lib/pages/auth/sign-in.dart
+++ lib/pages/auth/sign-in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smartbin/shared/theme.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
@@ -6,12 +7,37 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          'Sign In Page',
-        ),
-      ),
-    );
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 92),
+        child: Column(
+          children: [
+            Container(
+              width: 90,
+              height: 90,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: greenTransparentColor),
+            ),
+            SizedBox(height: 30),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Sign In',
+                    style: greenTs.copyWith(fontWeight: semiBold, fontSize: 32),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    textAlign: TextAlign.center,
+                    'It was popularised in the 1960s with the release of Letraset sheetscontaining Lorem Ipsum.',
+                    style: greyTs.copyWith(fontSize: 14),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/shared/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = const Color(0xff262626);
+Color whiteColor = const Color(0xffffffff);
+Color greenTransparentColor = const Color(0xffF4FFF5);
+Color greenColor = const Color(0xff09A811);
+Color greyColor = const Color(0xff61677D);
+
+TextStyle blackTs = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTs = GoogleFonts.poppins(color: whiteColor);
+TextStyle greenTs = GoogleFonts.poppins(color: greenColor);
+TextStyle greenTransparentTs =
+    GoogleFonts.poppins(color: greenTransparentColor);
+TextStyle greyTs = GoogleFonts.poppins(color: greyColor);
+
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight boldMax = FontWeight.w900;
